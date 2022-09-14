76d50abfa5c99416eb303542319d8d0161584731
Adrian Lopez <adallobi@gmail.com>
2022-09-13 21:09:10 -0400

    Added getRoundResult helper function

--- script.js
+++ script.js
@@ -26,45 +26,49 @@ function getPlayerChoice() {
     }
     return playerChoice;
 }
-
-function playRound(playerSelection, computerSelection) {
+function getRoundResult(playerSelection, computerSelection) {
     let result;
-
-    if(playerSelection === computerSelection) {
-        capitalize = playerSelection[0].toUpperCase();
-        playerSelection = playerSelection.replace(playerSelection[0], capitalize);
-        result = `It's a tie! ${playerSelection} ties ${computerSelection}`;
-        return result;
-    }
-    
-    switch(playerSelection) {
+    switch (playerSelection) {
         case 'rock':
             if (computerSelection === 'paper') {
                 result = 'You lose! Paper beats rock';
-            }
-            if (computerSelection === 'scissors') {
+            } else if (computerSelection === 'scissors') {
                 result = 'You win! Rock beats scissors';
             }
             break;
         case 'paper':
             if (computerSelection === 'rock') {
                 result = 'You win! Paper beats rock';
-            }
-            if (computerSelection === 'scissors') {
+            } else if (computerSelection === 'scissors') {
                 result = 'You lose! Scissors beats paper';
             }
             break;
         case 'scissors':
             if (computerSelection === 'rock') {
                 result = 'You lose! Rock beats scissors';
-            }
-            if (computerSelection === 'paper') {
+            } else if (computerSelection === 'paper') {
                 result = 'You win! Scissors beats paper';
             }
             break;
         default:
             console.log('iunno');
     }
+
+    return result;
+}
+
+function playRound(playerSelection, computerSelection) {
+    let result;
+
+    if (playerSelection === computerSelection) {
+        capitalize = playerSelection[0].toUpperCase();
+        playerSelection = playerSelection.replace(playerSelection[0], capitalize);
+        result = `It's a tie! ${playerSelection} ties ${computerSelection}`;
+        return result;
+    }
+    
+    result = getRoundResult(playerSelection, computerSelection);
+
     return result;
 }
 
